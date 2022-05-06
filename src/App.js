312c42f27ee2595bbe3864beefeaf5f9cb58b7f5
Oleksandr Shpak <oleksandr.shpak.pz.2020@lpnu.ua>
2022-05-06 13:34:28 +0300

    defined column size for different screensizes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
     return (
         <Container >
             <Row>
-                <Col className={'col-xl-5'}>
+                <Col className={'col-xxl-5 col-xl-6 col-lg-7 col-md-12 col-sm-12 col-12'}>
                     <PlayField/>
                 </Col>
-                <Col>
+                <Col className={'col-xxl-4 col-xl-4 col-lg-5 col-md-8 col-sm-12 col-12'}>
                     <div >
                         diagram here
                     </div>
                 </Col>
-                <Col className={'col-xl-3'}>
+                <Col className={'col-xxl-3 col-xl-3 col-lg-12 col-md-4 col-sm-12 col-12'}>
                     <FormComponent/>
 
                 </Col>
